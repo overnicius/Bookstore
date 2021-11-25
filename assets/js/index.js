ab5bf8215ee872c32e13aeef551612b3d13b1758
Vinicius Silva <vinicius_macedo_silva@hotmail.com>
2021-11-24 22:52:44 -0300

    Adicionando os arquivos

---
+++ assets/js/index.js
@@ -0,0 +1,179 @@
+
+/*irá remover ou adicionar a classe 'active', que irá mostrar ou não o form do search-btn*/
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+/*<--------------------------------------------------->*/
+
+/* irá remover ou adicionar o form de login através do login button*/
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.remove('active');
+}
+
+/*<--------------------------------------------------------------------->*/
+
+window.onscroll = () => {
+
+    searchForm.classList.remove('active');
+
+    if(window.scrollY > 80){
+        document.querySelector('.header', '.header-2').classList.add('active');
+    } else {
+        document.querySelector('.header', '.header-2').classList.remove('active');
+    }
+}
+
+window.onload = () => {
+
+    if(window.scrollY > 80){
+        document.querySelector('.header', '.header-2').classList.add('active');
+    } else {
+        document.querySelector('.header', '.header-2').classList.remove('active');
+    }
+}
+ fadeOut();
+
+ 
+/* função para o loader aparecer */
+
+function loader (){
+  document.querySelector('.loader-container').classList.add('active');
+}
+/* 'temporizador'para retirar o loader */
+function fadeOut(){
+  setTimeout(loader, 4000);
+}
+
+/*vai criar os sliders */
+
+var swiper = new Swiper(".books-slider", {
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 9500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        },
+      });
+
+var swiper = new Swiper(".featured-slider", {
+        spaceBetween: 10,
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 9500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          450:{
+              slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        },
+      });
+
+      var swiper = new Swiper(".arrivals-slider", {
+        spaceBetween: 10,
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 9500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          450:{
+              slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        },
+      });
+
+      var swiper = new Swiper(".reviews-slider", {
+        spaceBetween: 10,
+        loop: true,
+        grabCursor: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 9500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          450:{
+              slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        },
+      });
+
+      var swiper = new Swiper(".blogs-slider", {
+        spaceBetween: 10,
+        loop: true,
+        grabCursor: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 9500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          450:{
+              slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        },
+      });
